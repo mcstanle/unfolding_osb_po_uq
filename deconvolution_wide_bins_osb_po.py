c99a28c750c28789ca13e090d660f4f435d11c1a
Mike Stanley <mcstanle@andrew.cmu.edu>
2021-11-06 07:10:47 -0400

    added code for OSB and PO optimizations

--- deconvolution_wide_bins_osb_po.py
+++ deconvolution_wide_bins_osb_po.py
@@ -11,8 +11,111 @@ Created       : 06 Nov 2021
 Last Modified : 06 Nov 2021
 ===============================================================================
 """
+from interval_estimators import osb_interval, po_interval
 import numpy as np
+from tqdm import tqdm
 from utils import compute_coverage
+
+
+def run_osb_interval_exp(data, H, smear_means, K, alpha):
+    """
+    Compute an ensemble of bin-wise OSB intervals.
+
+    Parameters:
+    -----------
+        data        (np arr) : ensemble of data draws
+        H           (np arr) : collection of linear functionals
+        smear_means (np arr) : smear bin means to compute Cholesky decomp
+        K           (np arr) : smearing matrix
+        alpha       (np arr) : type 1 error threshold -- (1 - confidence level)
+
+    Returns:
+    --------
+        intervals (np arr) : the collection of optimized intervals
+    """
+    num_sims = data.shape[0]
+    num_funcs = H.shape[0]
+
+    intervals = np.zeros(shape=(num_sims, num_funcs, 2))
+
+    # find the change in basis
+    Sigma_data = np.diag(smear_means)
+    L_data = np.linalg.cholesky(Sigma_data)
+    L_data_inv = np.linalg.inv(L_data)
+
+    # transform the matrix
+    K_mc_tilde = L_data_inv @ K
+
+    for i in tqdm(range(num_sims)):
+        
+        # transform the data
+        data_i = L_data_inv @ data[i, :]
+        
+        for j in range(num_funcs):
+
+            # fit the intervals
+            interval_ij = osb_interval(
+                y=data_i, K=K_mc_tilde, h=H[j, :], alpha=alpha
+            )
+            
+            # save the intervals for later
+            intervals[i, j, :] = interval_ij
+
+    return intervals
+
+
+def run_po_interval_exp(prior, data, H, smear_means, K, alpha):
+    """
+    Compute an ensemble of bin-wise PO intervals.
+
+    NOTE: Since the PO intervals are essentially optimized without the data,
+    this function could be made substantially faster by performing the
+    optimization and then iterating over the data. One would use the
+    "return_int" argument in the po_interval function.
+
+    Parameters:
+    -----------
+        prior       (np arr) : prior used to optimize the intervals
+        data        (np arr) : ensemble of data draws
+        H           (np arr) : collection of linear functionals
+        smear_means (np arr) : smear bin means to compute Cholesky decomp
+        K           (np arr) : smearing matrix
+        alpha       (np arr) : type 1 error threshold -- (1 - confidence level)
+
+    Returns:
+    --------
+        intervals (np arr) : the collection of optimized intervals
+    """
+    num_sims = data.shape[0]
+    num_funcs = H.shape[0]
+
+    intervals = np.zeros(shape=(num_sims, num_funcs, 2))
+
+    # find the change in basis
+    Sigma_data = np.diag(smear_means)
+    L_data = np.linalg.cholesky(Sigma_data)
+    L_data_inv = np.linalg.inv(L_data)
+
+    # transform the matrix
+    K_mc_tilde = L_data_inv @ K
+
+    for i in tqdm(range(num_sims)):
+        
+        # transform the data
+        data_i = L_data_inv @ data[i, :]
+        
+        for j in range(num_funcs):
+
+            # fit the interval
+            interval_ij = po_interval(
+                y=data_i, prior_mean=prior_40, K=K_mc_tilde, h=H[j, :], alpha=alpha
+            )
+            
+            # save the intervals for later
+            intervals[i, j, :] = interval_ij
+
+    return intervals
+
 
 if __name__ == "__main__":
 
@@ -20,6 +123,10 @@ if __name__ == "__main__":
     GMM_ANSATZ = True
     ADVERSARIAL_ANSATZ = False
     READ_INTERVALS = True
+
+    # interval parameters
+    ALPHA = 0.05
+    NUM_EXP = 10
 
     # read in the true aggregated bin means
     t_means_w = np.load(file='./bin_means/gmm_wide.npz')['t_means_w']
@@ -34,7 +141,42 @@ if __name__ == "__main__":
             intervals_osb_fr = intervals_full_rank_gmm_ans_files['intervals_osb_fr']
             intervals_po_fr = intervals_full_rank_gmm_ans_files['intervals_po_fr']
         else:
-            pass
+            
+            # import the data
+            data = np.load(file='./data/wide_bin_deconvolution/simulation_data_ORIGINAL.npy')
+
+            # import the smearing matrix
+            K_fr_mc = np.load(
+                file='./smearing_matrices/K_full_rank_mats.npz'
+            )['K_fr_mc']
+
+            # import the bin means
+            bin_means_obj = np.load(file='./bin_means/gmm_fr.npz')
+            t_means_fr = bin_means_obj['t_means_fr']
+            s_means_fr = bin_means_obj['s_means_fr']
+
+            # import the aggregating functionals
+            H = np.load(file='./functionals/H_deconvolution.npy')
+
+            # OSB intervals
+            intervals_osb_fr = run_osb_interval_exp(
+                data=data[:NUM_EXP, :],
+                H=H,
+                smear_means=s_means_fr,
+                K=K_fr_mc,
+                alpha=ALPHA
+            )
+
+            # PO intervals
+            prior_40 = np.ones(40) * t_means_fr.mean()
+            intervals_po_fr = run_po_interval_exp(
+                prior=prior_40,
+                data=data[:NUM_EXP, :],
+                H=H,
+                smear_means=s_means_fr,
+                K=K_fr_mc,
+                alpha=ALPHA
+            )
 
         # estimate the coverage
         coverage_osb_fr = compute_coverage(
--- interval_estimators.py
+++ interval_estimators.py
@@ -11,6 +11,7 @@ Created       : 05 Nov 2021
 Last Modified : 06 Nov 2021
 ===============================================================================
 """
+import cvxpy as cp
 import numpy as np
 from scipy import stats
 
@@ -83,3 +84,215 @@ def least_squares_interval(K, h, y, alpha=0.05):
 
     return (lb, ub)
 
+
+def osb_interval(
+    y, K, h, A, alpha=0.05, verbose=False, options={}
+):
+    """
+    Compute OSB interval.
+
+    Dimension key:
+    - n : number of smear bins
+    - m : number of true bins
+
+    Parameters:
+    -----------
+        y         (np arr) : cholesky transformed data -- n x 1
+        K         (np arr) : cholesky transformed matrix -- n x m
+        h         (np arr) : functional for parameter transform -- m x 1
+        A         (np arr) : Matrix to enforce non-trivial constraints
+        alpha     (float)  : type 1 error threshold -- (1 - confidence level)
+        options   (dict)   : ECOS options for cvxpy
+
+    Returns:
+    """
+    m = K.shape[1]
+
+    # find the slack factor
+    x = cp.Variable(m)
+    cost = cp.sum_squares(y - K @ x)
+    prob = cp.Problem(
+        objective=cp.Minimize(cost),
+        constraints=[
+            A @ x <= 0
+        ]
+    )
+    s2 = prob.solve(solver='ECOS', verbose=verbose, **options)
+
+    # find the constraint bound
+    sq_err_constr = np.square(
+        stats.norm(loc=0, scale=1).ppf(1 - (alpha / 2))
+    ) + s2
+
+    # define a variables to solve the problem
+    x_lb = cp.Variable(m)
+    x_ub = cp.Variable(m)
+
+    # define the problem
+    prob_lb = cp.Problem(
+        objective=cp.Minimize(h.T @ x_lb),
+        constraints=[
+            cp.square(cp.norm2(y - K @ x_lb)) <= sq_err_constr,
+            A @ x_lb <= 0
+        ]
+    )
+    prob_ub = cp.Problem(
+        objective=cp.Minimize(-h.T @ x_ub),
+        constraints=[
+            cp.square(cp.norm2(y - K @ x_ub)) <= sq_err_constr,
+            A @ x_ub <= 0
+        ]
+    )
+
+    # solve the problem
+    opt_lb = prob_lb.solve(solver='ECOS', verbose=verbose, **options)
+    opt_ub = prob_ub.solve(solver='ECOS', verbose=verbose, **options)
+
+    # check for convergence
+    assert 'optimal' in prob_lb.status
+    assert 'optimal' in prob_ub.status
+
+    return opt_lb, -opt_ub
+
+
+def prior_interval_construct(y, opt_x_lb, opt_x_ub, b, alpha, m):
+    """
+    Given the atomic pieces from the optimization, this function constructs
+    the actual interval.
+
+    Parameters:
+        y        (np arr) : observed bin counts - m elements
+        opt_x_lb (np arr) : optimized lb x vector - first m elements are w,
+                            next n elements are c
+        opt_x_ub (np arr) : optimized ub x vector - first m elements are w,
+                            next n elements are c
+        b        (np arr) : n length vector -- part of physical constraints
+        alpha    (float)  : in [0, 1], confidence level of interval
+        m        (int)    : number of smear bins
+
+    Return:
+        tuple of optimized lower and upper endpoints
+    """
+    w_lb = opt_x_lb[:m].copy()
+    c_lb = opt_x_lb[m:].copy()
+    w_ub = opt_x_ub[:m].copy()
+    c_ub = opt_x_ub[m:].copy()
+
+    # compute Gaussian quantile
+    z_alpha = stats.norm.ppf(1 - alpha/2)
+
+    # compute bounds
+    lb = np.dot(w_lb, y) - z_alpha * np.linalg.norm(w_lb) - np.dot(b, c_lb)
+    ub = np.dot(w_ub, y) + z_alpha * np.linalg.norm(w_ub) + np.dot(b, c_ub)
+
+    return lb, ub
+
+
+def po_interval(
+    y,
+    prior_mean,
+    K,
+    h,
+    A,
+    alpha=0.05,
+    return_int=True,
+    verbose=False,
+    return_cvx_obj=False,
+    options={'max_iters': 100, 'abstol': 1e-8, 'reltol': 1e-8, 'feastol': 1e-8}
+):
+    """
+    Prior interval optimization.
+
+    Dimension key
+    - m : smear space dimension
+    - n : unfolded space dimension
+    - q : number of inequality constraints
+
+    NOTE: the optimized variables, x_lb and x_ub, are oriented such that the
+    first m elements correspond to w and the next q elements correspond to c.
+
+    Options for the ECOS algo: https://www.cvxpy.org/tutorial/advanced/index.html
+
+    Parameters:
+        y              (np arr) : m x 1 
+        prior_mean     (np arr) : n x 1
+        K              (np arr) : smearing matrix -- m x n
+        h              (np arr) : functional -- n x 1
+        A              (np arr) : constraint matrix -- q x n
+        alpha          (float)  : confidence level
+        return_int     (bool)   : toggle to return interval or optimized variables
+        verbose        (bool)   : returns the output of the optimization
+        return_cvx_obj (bool)   : return the cvxpy objects
+        options        (dict)   : options for the ECOS algo (see above for link)
+
+    Returns
+        if return_int:
+            tuple : (lower bound, upper bound)
+        else:
+            optimized vectors x_lb.value, x_ub.value
+    """
+    # define the new algebra objects
+    m, n = K.shape
+    q = A.shape[0]
+
+    P = np.zeros(shape=(m + q, m + n))
+    gamma = np.zeros(m + n)
+    B = np.zeros(shape=(m + q, m + q))
+    F = np.zeros(shape=(q, m + q))
+    D_lb = np.zeros(shape=(n, m + q))
+    D_ub = np.zeros(shape=(n, m + q))
+
+    P[:m, :n] = K.copy()
+
+    D_lb[:, :m] = - K.copy().T
+    D_lb[:, m:] = A.copy().T
+    D_ub[:, :m] = - K.copy().T
+    D_ub[:, m:] = - A.copy().T
+
+    gamma[:n] = prior_mean.copy()
+    B[:m, :m] = np.identity(m)
+    F[:, m:] = np.identity(q)
+
+    # compute the normal quantile
+    norm_quant = stats.norm.ppf(1 - alpha/2)
+
+    # set up optimizations
+    x_lb = cp.Variable(m + q)
+    x_ub = cp.Variable(m + q)
+
+    prob_lb = cp.Problem(
+        objective=cp.Maximize(x_lb.T @ P @ gamma - norm_quant * cp.norm(B.T @ x_lb)),
+        constraints=[
+            h + D_lb @ x_lb == np.zeros(n),
+            F @ x_lb >= np.zeros(q)
+        ]
+    )
+    prob_ub = cp.Problem(
+        objective=cp.Minimize(x_ub.T @ P @ gamma + norm_quant * cp.norm(B.T @ x_ub)),
+        constraints=[
+            h + D_ub @ x_ub == np.zeros(n),
+            F @ x_ub >= np.zeros(q)
+        ]
+    )
+
+    # solve the problems
+    opt_lb = prob_lb.solve(verbose=verbose, **options)
+    opt_ub = prob_ub.solve(verbose=verbose, **options)
+
+    # check convergence
+    assert 'optimal' in prob_lb.status
+    assert 'optimal' in prob_ub.status
+
+    if return_int:
+        return prior_interval_construct(
+            y=y,
+            opt_x_lb=x_lb.value,
+            opt_x_ub=x_ub.value,
+            b=np.zeros(q),
+            alpha=alpha,
+            m=m
+        )
+    elif return_cvx_obj:
+        return prob_lb, prob_ub
+    else:
+        return x_lb.value, x_ub.value
